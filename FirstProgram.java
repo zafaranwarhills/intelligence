e47df31c87f8ff284d651431b318bf7c96fc7ef9
Zafar Anwar <zafaranwar.dec79@gmail.com>
2018-06-23 16:05:24 -0400

    removed one line

--- FirstProgram.java
+++ FirstProgram.java
@@ -4,7 +4,6 @@ public class FirstProgram	{
 	public static void main(String args[])	{
 
 		System.out.println("This is my first program");
-		System.out.println("This is my first program");
 
 	}
 }
