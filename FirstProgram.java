d69f67dba078d966424027dc846d66d3076ec2ea
zafaranwarhills <40525257+zafaranwarhills@users.noreply.github.com>
2018-06-23 16:10:07 -0400

    Update FirstProgram.java

--- FirstProgram.java
+++ FirstProgram.java
@@ -4,7 +4,7 @@ public class FirstProgram	{
 	public static void main(String args[])	{
 
 		System.out.println("This is my first program");
-		System.out.println("This is my first program");
+		
 
 	}
 }
